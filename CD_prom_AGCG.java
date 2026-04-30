611972db3d04db18fe794b47a8439b18d5e31a4e
Giovani-Guzman <angel.can_pm25231@cecyte.edu.mx>
2026-04-30 06:01:42 -1200

    Add files via upload

---
+++ CD_prom_AGCG.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Alumno
+ */
+public class CD_prom_AGCG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+  Scanner scanner = new Scanner (System.in);    
+  
+  System.out.print("ingresa el promedio del estudiante: ") ;    
+  double prom = scanner.nextDouble();
+  
+  if (prom >= 7.0) {
+      System.out.print("El estudiante aprobo.");
+  } else {
+  System.out.print("el estudiante reprobo");}
+             
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+      
+    }
+    
+}
